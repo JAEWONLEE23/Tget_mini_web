bc9c7d687acc58c783908de33bc3933d3ed4416d
COM <COM@DESKTOP-L2F7LSG>
2024-04-05 15:26:38 +0900

    코드 수정

--- src/main/java/com/mycompany/Tget_mini_web/HomeController.java
+++ src/main/java/com/mycompany/Tget_mini_web/HomeController.java
@@ -11,7 +11,8 @@ import lombok.extern.slf4j.Slf4j;
 public class HomeController {
 	@RequestMapping("")
 	public String index() {
-		log.info("index() 실행");
+		log.info("index() 실행1");
+		log.info("index() 실행2");
 		return "home";
 	}
 }
